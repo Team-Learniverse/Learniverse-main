e9904fbaf3fea742c0cdcba14177500a4ab5d009
whitepaper907 <101ghgh@naver.com>
2023-09-13 19:35:44 +0900

    fix : cors 설정 수정

---
+++ src/main/java/learniverse/learniversemain/config/CorsFilter.java
@@ -0,0 +1,41 @@
+package learniverse.learniversemain.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CorsFilter extends OncePerRequestFilter {
+    // 허용하는 Origin 목록
+    private static final List<String> ALLOWED_ORIGINS = new ArrayList<>(
+            Arrays.asList(
+                    "https://learniverse-front-end.vercel.app",
+                    "http://localhost:3000"
+            )
+    );
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String origin = request.getHeader("Origin");
+        // Origin 검증
+        // 검증 후 헤더 추가
+        response.addHeader("Access-Control-Allow-Origin", origin);
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "Authorization");
+        response.setIntHeader("Access-Control-Max-Age", 3600);
+
+        if(ALLOWED_ORIGINS.contains(origin))
+            filterChain.doFilter(request, response);
+    }
+}
