a5e68c8e5cc64cafd27c421cb1774a5ddb83f891
whitepaper907 <101ghgh@naver.com>
2023-10-11 17:04:39 +0900

    fix : created date 추가

--- src/main/java/learniverse/learniversemain/entity/mongoDB/JoinsEntity.java
+++ src/main/java/learniverse/learniversemain/entity/mongoDB/JoinsEntity.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Document(collection = "joins")
@@ -14,11 +15,15 @@ public class JoinsEntity {
     private boolean isDefault;
     private LocalDate pinDate;
     private int enterRoom;
+    private LocalDate createdDate;
 
     public JoinsEntity(Long memberId, long roomId, boolean isDefault) {
         this.memberId = memberId;
         this.roomId = roomId;
         this.isDefault = isDefault;
         this.enterRoom = 0;
+        LocalDateTime now = LocalDateTime.now();
+        LocalDate localDate = now.toLocalDate();
+        this.createdDate = localDate;
     }
 }
