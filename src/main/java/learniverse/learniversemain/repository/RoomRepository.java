13a25010b3ec9ed3564d1b0708c0a37e73981ffb
whitepaper907 <101ghgh@naver.com>
2023-10-28 15:41:27 +0900

    fix : 해시태그 테이블 조인 후 검색

--- src/main/java/learniverse/learniversemain/repository/RoomRepository.java
+++ src/main/java/learniverse/learniversemain/repository/RoomRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,4 +19,7 @@ public interface RoomRepository extends JpaRepository<RoomEntity, Long> {
     Page<RoomEntity> findByRoomCategoryAndRoomNameContainingOrRoomIntroContaining(int category, String str, String str1, Pageable pageable);
 
     Page<RoomEntity> findByRoomCategoryAndRoomNameContainingOrRoomCategoryAndRoomIntroContaining(int category, String str, int category1, String str1, Pageable pageable);
+
+    @Query("SELECT DISTINCT r FROM rooms r LEFT JOIN r.hashtags h WHERE h.hashtag LIKE %:hashtag%")
+    Page<RoomEntity> findByHashtagsHashtagContaining(@Param("hashtag") String hashtag, Pageable pageable);
 }
