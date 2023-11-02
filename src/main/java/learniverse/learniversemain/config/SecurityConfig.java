8763e73489f83e97d60b0efa6d388280f42c75af
uujiinn <jane9994@gmail.com>
2023-11-03 00:43:46 +0900

    feat: 로그인에러

--- src/main/java/learniverse/learniversemain/config/SecurityConfig.java
+++ src/main/java/learniverse/learniversemain/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
                         )
                         .successHandler(oAuth2SuccessHandler)
                 );
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
